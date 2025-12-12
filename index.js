96d1abddc4051387ce5391d1beca830bfe69443b
mohit8233 <mohitbanna085@gmail.com>
2025-12-12 20:23:28 +0530

    breakpoints

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 // toggle icon navbar
-
-
-
 // scroll sections 
 
-let sections = document.querySelector('section')
-let navlinks = document.querySelector(' header nav a')
+let sections = document.querySelectorAll("section")
+let navlinks = document.querySelectorAll("header nav a")
 
 
 window.onscroll = () =>{
     sections.forEach(sec => {
-        let top = window.screenY
+        let top = window.scrollY
         let offset = sec.offsetTop -100
         let height = sec.offsetHeight
         let id = sec.getAttribute('id')
@@ -19,14 +16,53 @@ window.onscroll = () =>{
 
             // active navbar link 
             navlinks.forEach(links => {
-                links.classList.remove('active' )
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+                links.classList.remove('active')
+                document
+                        .querySelector('header nav a[href*='+ id +'] ')
+                        .classList.add('active')
             })
         }
     })
     // sticky header
-
     let header = document.querySelector('header')
 
     header.classList.toggle('sticky', window.scrollY > 100)
 }
+
+
+// // toggle icon navbar
+// // scroll sections 
+
+// let sections = document.querySelectorAll("section"); 
+// let navlinks = document.querySelectorAll("header nav a"); 
+
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 100;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute("id");
+
+//         if (top >= offset && top < offset + height) {
+//             // active navbar link 
+//             navlinks.forEach(link => {
+//                 link.classList.remove("active");
+//             });
+
+//             document
+//                 .querySelector("header nav a[href*=" + id + "]")
+//                 .classList.add("active");
+//         }
+//     });
+
+//     // sticky header
+//     let header = document.querySelector("header");
+//     header.classList.toggle("sticky", window.scrollY > 100);
+// };
+
+
+
+
+
+
+
